10f37d4206279e15bab0a1f1ebc0fdb6ac9f04b3
rashed.bishal <rashed.bishal@gmail.com>
2018-01-15 11:19:18 -0600

    Binary Search

---
+++ BinarySearch.c
@@ -0,0 +1,40 @@
+// Binary Search Example
+#include <iostream>
+#include <string>
+
+int doBinarySearch(int arr[], int fetch, int left, int right)
+{
+    
+    while(left <= right)
+    {
+        int midpoint = left + ((right - left) / 2);
+    
+        if(arr[midpoint] == fetch)
+        {
+            return fetch;   
+        }
+        else if(fetch < arr[midpoint])
+        {
+            right = midpoint - 1;
+            //return doBinarySearch(arr, fetch, left, midpoint - 1);   
+        }
+        else if(fetch > arr[midpoint])
+        {
+            left = midpoint + 1;
+            //return doBinarySearch(arr, fetch, midpoint+1, right);   
+        }
+    }
+    
+    return -1;
+}
+
+
+int main()
+{
+  int arr[] = {1,2,3,4,5};
+  int fetch = 5;
+  
+  int result = doBinarySearch(arr, fetch, 0, sizeof(arr)/sizeof(arr[0]));
+  
+  printf("%d",result);
+}
