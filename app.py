8d217f2afc6e329e77fe87ea86b927d5ef4daf6c
rashed-bishal <rashed.bishal@gmail.com>
2018-02-07 12:37:10 +0600

    'updated'

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask
 from flask import request
-from flask_cors import CORS
+#from flask_cors import CORS
 #from pymongo import MongoClient
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
 @app.route('/')
 def homepage():
