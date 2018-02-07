1894857ad0394eee899444943b443b8d8be22c81
rashed-bishal <rashed.bishal@gmail.com>
2018-02-07 13:03:31 +0600

    'red'

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 from flask import request
 #from flask_cors import CORS
-#from pymongo import MongoClient
+from pymongo import MongoClient
 
 app = Flask(__name__)
 #CORS(app)
