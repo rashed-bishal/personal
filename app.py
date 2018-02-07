ca185788b10ca44def8ed22036cc453f8163d490
rashed-bishal <rashed.bishal@gmail.com>
2018-02-07 11:45:00 +0600

    'changes'

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-   return 'Hello World’
+def homepage():
+    return "Hello"
 
 if __name__ == '__main__':
-   app.run()
+    app.run(debug=True, use_reloader=True)
