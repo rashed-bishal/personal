94611ccd7cec88e362d8121421d16f863f81fa98
rashed-bishal <rashed.bishal@gmail.com>
2018-02-07 11:31:02 +0600

    'Edited'

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
-import os
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import flask
+from flask import request
+from flask import jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
-CORS(app)
 
 client = MongoClient('mongodb://admin:root@ds125618.mlab.com:25618/mydatabase')
-db=client.get_database('mydatabase')
+db=client.mydatabase;
 
 @app.route('/')
 def homepage():
-    return "BANGLADESH"
-
-#@app.route('/value',methods = ['GET','POST'])
-#def value():
-
-    #result=db.gasReadings.insert_one(content)
-    #return result;
+    return "MILKYWAY"
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
